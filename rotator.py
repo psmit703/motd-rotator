0d18ed7c19ee2ccf60d39df45121c42d5df78f0a
Pete Smith <105141656+psmit703@users.noreply.github.com>
2024-06-25 19:30:13 -0400

    added a birthday option to rotator.py

--- rotator.py
+++ rotator.py
@@ -3,9 +3,14 @@ from datetime import datetime as dt
 import os
 import random
 
+birthMonth = 0  # 1-12, 0 if not applicable, set this to the month you were born to get a special message on your birthday
+birthDay = 0  # 1-31, 0 if not applicable, set this to the day of the month you were born to get a special message on your birthday
+
 messages = json.loads(open("motds.json", "r").read())
 
-if dt.now().month == 6:
+if dt.now().month == birthMonth and dt.now().day == birthDay:
+    thisMessage = messages["birthday"]
+elif dt.now().month == 6:
     thisMessage = messages["prideMonth"]
 else:
     thisMessage = random.choice(messages["motds"])
